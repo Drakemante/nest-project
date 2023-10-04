05e82fc3a29dc17bd23c337e03bf232a0283f8b1
Drakemante <samoelcostaviza@gmail.com>
2023-10-03 21:03:11 -0300

    feat :add api tags of auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,33 +2,46 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: "Criar novo usuário"
+  })
   @Post()
   create(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
   }
 
   @Get()
+  @ApiOperation({summary: 'Pesquisar todos os usúarios'})
   findAll() {
     return this.authService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({summary: 'Pesquisar um usúario por id'})
   findOne(@Param('id') id: string) {
     return this.authService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({summary: 'Alterar dados de usúario'})
   update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
     return this.authService.update(+id, updateAuthDto);
   }
 
   @Delete(':id')
+  @ApiOperation({summary: 'Deletar um usúario'})
   remove(@Param('id') id: string) {
     return this.authService.remove(+id);
   }
 }
+function ApiTags(arg0: string): (target: typeof AuthController) => void | typeof AuthController {
+  throw new Error('Function not implemented.');
+}
+
