14b87e9be90876772a8807cb13d109c1afc7b642
Drakemante <samoelcostaviza@gmail.com>
2023-10-04 21:50:21 -0300

    feat: cofig validation auth creat

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { setDefaultAutoSelectFamilyAttemptTimeout } from 'net';
 import { config } from 'rxjs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const port = 3000 || process.env.PORT
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe())
 
   const config = new DocumentBuilder().setTitle('Nest-Project').setVersion("1.0.0").addTag('Auth').addServer('http://localhost:3000', 'Local').build();
 
