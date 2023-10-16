254d816611f969fe77a72ba06a2ec454c962fc50
Drakemante <samoelcostaviza@gmail.com>
2023-10-16 18:54:05 -0300

    Feat: Crud

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,8 @@
-export class User {}
+export class User {
+    id: string;
+    email: string;
+    name: string;
+    password: string;
+    picture: string;
+    admin?: boolean;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,48 +2,45 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService as UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @ApiOperation({
-    summary: "Criar novo usuário"
-  })
   @Post()
-  @ApiOperation({summary: 'Criar novo usuário',})
+  @ApiOperation({summary: 'Criar novo usuário.',})
   create(@Body() createAuthDto: CreateUserDto) {
-    try {
-      return this.userService.create(createAuthDto);
-    } catch (error) {
-      console.log(error)
-    }
+   try {
+     return this.userService.create(createAuthDto);
+   } catch (error) {
+    console.log(error)
+   }
   }
 
   @Get()
-  @ApiOperation({summary: 'Pesquisar todos os usúarios'})
+  @ApiOperation({summary: 'Pesquisar todos os usuários.'})
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
-  @ApiOperation({summary: 'Pesquisar um usúario por id'})
+  @ApiOperation({summary: 'Pesquisar um usuário por id.'})
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    return this.userService.findOne(id);
   }
 
   @Patch(':id')
-  @ApiOperation({summary: 'Alterar dados de usúario'})
+  @ApiOperation({summary: 'Alterar dados do usuário.'})
   update(@Param('id') id: string, @Body() updateAuthDto: UpdateUserDto) {
-    return this.userService.update(+id, updateAuthDto);
+    return this.userService.update(id, updateAuthDto);
   }
 
   @Delete(':id')
-  @ApiOperation({summary: 'Deletar um usúario'})
+  @ApiOperation({summary: 'Deletar um Usuário.'})
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.userService.remove(id);
   }
 }
 
