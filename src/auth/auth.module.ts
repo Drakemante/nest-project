9e9a1882e41ba2fec6430926f268fe7fca4c4141
Drakemante <samoelcostaviza@gmail.com>
2023-10-16 20:58:15 -0300

    Feat: add controller auth and local-auth.guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserService } from 'src/user/user.service';
 
 @Module({
-  imports:[UserModule],
-  providers: [AuthService]
+  imports:[PrismaModule],
+  providers: [AuthService, UserService]
 })
 export class AuthModule {}
